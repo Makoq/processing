b79adb9d042acd84bd9e2775f4bc532d34a68f98
unknown <913088741@qq.com>
2020-11-14 19:25:27 +0800

    processing methods

---
+++ processingTemplate.py
@@ -0,0 +1,10 @@
+import sys
+import os #导入模块
+# input,output分别为输入、输出数据集文件夹目录，此外是处理方法参数，如这里的CityName（可选项）
+def execute(input,output,cityName):
+    # 定义处理中间件，可调用外部script,exe,lib,api等
+    # do something
+if __name__ == "__main__":
+    if execute(sys.argv[1],sys.argv[2],sys.argv[3]) is True:
+        print("ok")
+      
